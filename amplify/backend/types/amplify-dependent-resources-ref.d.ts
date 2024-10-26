93ff5cfedbf151aa961ea330dce4dc5f345d2775
root <root@ip-10-132-46-205.eu-north-1.compute.internal>
2024-10-26 16:00:14 +0000

    feat: graph ql added

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "mantisfreereactadmin": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "storage": {
     "dynamodsp": {
       "Arn": "string",
