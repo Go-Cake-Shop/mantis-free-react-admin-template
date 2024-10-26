951081b71eb3954b636193fa62c30b93fdae93d0
root <natananshiferaw@gmail.com>
2024-10-26 12:21:11 +0000

    Added DynamoDB to Amplify stack

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+  "storage": {
+    "dynamodsp": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
+    }
+  }
+}
